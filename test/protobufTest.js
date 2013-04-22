639209a0f5c309d933714fd70cf70bef18aaddad
Xiaogang Zhang <zhang0925@gmail.com>
2013-04-22 11:01:05 +0800

    remove un used file and fix bug for error tag

--- test/protobufTest.js
+++ test/protobufTest.js
@@ -12,7 +12,14 @@ describe('msgEncoderTest', function(){
 		for(var route in tc){
 			var msg = tc[route];
 			var buffer = protobuf.encode(route, msg);
+
+console.log(msg);
+			console.log(buffer.length);
+			console.log(buffer)
+
 			var decodeMsg = protobuf.decode(route, buffer);
+
+			console.log(decodeMsg);
 
 			util.equal(msg, decodeMsg).should.equal(true);
 		}
